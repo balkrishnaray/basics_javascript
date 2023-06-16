e9ebcd438cf16bc38e71fd40c85db47bf6f44e85
balkrishnray <balkrishnaroy31@gmail.com>
2023-06-16 18:20:03 +0545

    javascript basics

---
+++ index.js
@@ -0,0 +1,62 @@
+// console.log('Hello World! This is Himalaya college of Engineering');
+// var x ='4564';
+// console.log(x);
+
+//  let b = '123';
+// console.log(b);
+
+// const a = 234;
+// console.log(a);
+
+// // alert("Warning");
+// // const age = prompt('How old are you?');
+// // console.log(age);
+
+// const isBoss = confirm("Are you the boss");
+// console.log(isBoss);
+
+// let x = 2;
+// const y = 2;
+// if(x==1){
+//     console.log("value of x is 1");
+// }
+// if ( (x=1) || (y = 4))  
+// {
+//     console.log("Operator tested");
+// }
+// if(!x){
+//     console.log("x is not true");
+// }
+
+// console.log('assaa');
+
+// if(x==1){
+//     console.log("X is 1");
+// }
+// else{
+//     console.log("x is not 1");
+// }
+//  let result = x === 1 ? "x is 1" : "x is not 1"; 
+//  console.log(result);
+
+// for (let i = 0 ; i<10 ; i++){
+//     console.log(i);
+// }
+
+// function add(a,b){
+//     //return a + b;
+//     console.log(a + b);
+// }
+
+// add(3,4);
+// add(5,6);
+
+function area(a,b){
+    return a * b;
+}
+
+area(3,4);
+area(5,4); 
+
+let data = 1;
+console.log(data);

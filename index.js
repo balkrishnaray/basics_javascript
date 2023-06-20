5559a9921ed29b8105e7ddf8eb4ab342f5c0ab01
balkrishnray <balkrishnaroy31@gmail.com>
2023-06-20 13:42:40 +0545

    objects

--- index.js
+++ index.js
@@ -71,72 +71,91 @@
 // const c =4;
 // console.log(c);
 
-console.log("hello world");
+// console.log("hello world");
 
-const pi = 3.14;
-console.log(pi);
+// const pi = parseFloat( prompt("Set the value of PI"));
+// console.log("value of PI is " + pi);
 
-let a = parseInt(prompt("Enter the first number: ")), 
-b = parseInt(prompt("Enter the second number: ")) ;
-let z = (a + b);
+// let a = parseInt(prompt("Enter the first number: ")), 
+// b = parseInt(prompt("Enter the second number: "));
+// let z = (a + b);
 
- if(a==b){
-console.log("The tripled sum is " + 3*z);
- }
- else{
+//  if(a==b){
+// console.log("The tripled sum is " + 3*z);
+//  }
+//  else{
    
-    console.log("The sum is " + z);
- }
+//     console.log("The sum is " + z);
+//  }
 
- const c = Number(prompt('Enter a number'));
-if(c%3 == 0 && c%7 == 0){
-    console.log('It is a multiple of both 3 and 7');
-}
-else if(c%3 == 0){
-    console.log('It is a multiple of 3');
-}
-else if(c%7 == 0){
-    console.log('It is a multiple of 7');
-}
-else{
-    console.log('It is not a multiple of 3 or 7');
-}
+//  const c = Number(prompt('Enter a number'));
+// if(c%3 == 0 && c%7 == 0){
+//     console.log('It is a multiple of both 3 and 7');
+// }
+// else if(c%3 == 0){
+//     console.log('It is a multiple of 3');
+// }
+// else if(c%7 == 0){
+//     console.log('It is a multiple of 7');
+// }
+// else{
+//     console.log('It is not a multiple of 3 or 7');
+// }
 
-console.log(Date());
+// console.log(Date());
 
-const x = Number(prompt('Enter a number'));
-const y = Number(prompt('Enter a number'));
-const mnd = (a, b) =>
-{
-    console.log(a*b);
-    console.log(a/b);
-}
-mnd(x, y);
+// const x = Number(prompt('Enter a number'));
+// const y = Number(prompt('Enter a number'));
+// function cal_mul_div(a, b)
+// {
+//     console.log(a*b);
+//     console.log(a/b);
+// }
+// cal_mul_div(x, y);
 
-//temperature in celcius and fahrenheit
-const oldC = Number(prompt('Enter temperature in ℃'));
-const oldF = Number(prompt('Enter temperature in ℉'));
-const newC = ((oldF-32)/9)*5;
-const newF = ((oldC/5)*9)+32;
-console.log(oldC+'℃ is '+newF+'℉');
-console.log(oldF+'℉ is '+newC+'℃');
+// //temperature in celcius and fahrenheit
+// const oldC = Number(prompt('Enter temperature in ℃'));
+// const oldF = Number(prompt('Enter temperature in ℉'));
+// const newC = ((oldF-32)/9)*5;
+// const newF = ((oldC/5)*9)+32;
+// console.log(oldC+'℃ is '+newF+'℉');
+// console.log(oldF+'℉ is '+newC+'℃');
 
-//Counting number of vowels in a string
-const f = prompt('Enter a string');
-const countVowels = (str) =>
-{
-    const vowels = 'AaEeIiOoUu';
-    let count = 0;
-    for(const char of str)
-    {
-        if(vowels.includes(char))
-        {
-            count++;
-        }
-    }
-    console.log(count);
-}
-countVowels(f);
+// //Counting number of vowels in a string
+// const f = prompt('Enter a string');
+// const countVowels = (str) =>
+// {
+//     const vowels = 'AaEeIiOoUu';
+//     let count = 0;
+//     for(const char of str)
+//     {
+//         if(vowels.includes(char))
+//         {
+//             count++;
+//         }
+//     }
+//     console.log(count);
+// }
+// countVowels(f);
+
+// const area = (a,b) =>{
+//     let d = a * b;
+//     return d;
+// }
+// area(1,2);
+//  console.log(d);
+
+ const person ={
+    firstname:"balkrishna",
+    lastname:"Ray",
+    fullname: ()=>{
+        return `${person.firstname} ${person.lastname}`;
+    },
+ };
+ const c = person.firstname;
+ const a = person.fullname();
+ console.log(c,a);
+
 
  
 

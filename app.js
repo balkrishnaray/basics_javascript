053921e0828218b50d40fd12d18b2b3bd9b205e3
Balkrishna <86654108+balkrishnaray@users.noreply.github.com>
2023-06-21 18:16:48 +0545

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-http.createServer ((request,response) => {
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end('Hello World.');
-    
-})
-.listen(8080);
-console.log('Application running on port 8080');
